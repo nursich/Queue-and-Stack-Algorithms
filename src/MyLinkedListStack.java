2bd702b7489f33aee3e5feae591a46865df848cb
nursich <nurlibaiem04@mail.ru>
2023-05-01 17:00:26 +0600

    MyLinkedListStack: Creating constructor

--- src/MyLinkedListStack.java
+++ src/MyLinkedListStack.java
@@ -2,4 +2,9 @@ import java.util.EmptyStackException;
 import java.util.LinkedList;
 
 public class MyLinkedListStack<T> {
+    private LinkedList<T> stack;
+
+    public MyLinkedListStack() {
+        stack = new LinkedList<>();
+    }
 }
