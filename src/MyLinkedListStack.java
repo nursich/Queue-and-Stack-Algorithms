c558bf8499722764e8e25515c798da60bd0a56ce
nursich <nurlibaiem04@mail.ru>
2023-05-01 17:51:54 +0600

    Documentation of code for MyLinkedListStack

--- src/MyLinkedListStack.java
+++ src/MyLinkedListStack.java
@@ -2,35 +2,35 @@ import java.util.EmptyStackException;
 import java.util.LinkedList;
 
 public class MyLinkedListStack<T> {
-    private LinkedList<T> stack;
+    private LinkedList<T> stack; // Instance variable
 
-    public MyLinkedListStack() {
+    public MyLinkedListStack() {    // Constructs a new MyLinkedListStack object with an empty linked list.
         stack = new LinkedList<>();
     }
 
-    public void push(T data) {
-        stack.addFirst(data);
+    public void push(T data) {   // Pushes the specified element onto the top of this stack.
+        stack.addFirst(data);    // @param element the element to push onto this stack
     }
 
-    public boolean isEmpty() {
-        return stack.isEmpty();
+    public boolean isEmpty() {    // Tests if this stack is empty.
+        return stack.isEmpty();   // @return true if and only if this stack contains no items; false otherwise
     }
 
-    public T pop() {
+    public T pop() {     // Removes the element at the top of this stack and returns that element.
         if (isEmpty()) {
-            throw new EmptyStackException();
+            throw new EmptyStackException();   // @throws EmptyStackException if this stack is empty
         }
-        return stack.removeFirst();
+        return stack.removeFirst();   // @return the element at the top of this stack
     }
 
-    public T peek() {
+    public T peek() {     // Looks at the element at the top of this stack without removing it from the stack.
         if (isEmpty()) {
-            throw new EmptyStackException();
+            throw new EmptyStackException();    // @throws EmptyStackException if this stack is empty
         }
-        return stack.peekFirst();
+        return stack.peekFirst();    // @return the element at the top of this stack
     }
 
-    public int size() {
+    public int size() {     // Returns the number of elements in this stack.
         return stack.size();
     }
 }
