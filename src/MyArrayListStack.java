430d9af49d663b66232fa7a8b6318441256cd511
nursich <nurlibaiem04@mail.ru>
2023-05-01 16:10:51 +0600

    MyArrayListStack: Creating constructor

--- src/MyArrayListStack.java
+++ src/MyArrayListStack.java
@@ -1,5 +1,9 @@
 import java.util.EmptyStackException;
 import java.util.ArrayList;
-public class MyArrayListStack {
+public class MyArrayListStack<E> {
+    private ArrayList<E> stack;
 
+    public MyArrayListStack() {
+        stack = new ArrayList<>();
+    }
 }
