f27e08c8f67c6c10ca91bf5b879ec7f72bbe17b1
nursich <nurlibaiem04@mail.ru>
2023-05-01 17:27:55 +0600

    MyLinkedListQueue: Creating constructor

--- src/MyLinkedListQueue.java
+++ src/MyLinkedListQueue.java
@@ -2,5 +2,9 @@ import java.util.NoSuchElementException;
 import java.util.LinkedList;
 
 public class MyLinkedListQueue<T> {
+    private LinkedList<T> queue;
 
+    public MyLinkedListQueue() {
+        queue = new LinkedList<>();
+    }
 }
