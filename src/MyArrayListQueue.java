6537f2172db8ffd30e08da6bb9e0ab6d6d3c72f8
nursich <nurlibaiem04@mail.ru>
2023-05-01 17:48:04 +0600

    Documentation of code for MyArrayListQueue

--- src/MyArrayListQueue.java
+++ src/MyArrayListQueue.java
@@ -1,36 +1,41 @@
 import java.util.NoSuchElementException;
 import java.util.ArrayList;
 
-public class MyArrayListQueue<E> {
-    private ArrayList<E> queue;
+/**
+ MyArrayListQueue is a class that implements a queue using an ArrayList data structure.
+ It contains methods for enqueueing, dequeue, peeking, checking if the queue is empty, and getting the size.
+ */
 
-    public MyArrayListQueue() {
+public class MyArrayListQueue<E> {
+    private ArrayList<E> queue;  // Instance variable
+
+    public MyArrayListQueue() {     // Constructor for MyArrayListQueue that initializes an empty ArrayList
         queue = new ArrayList<E>();
     }
 
-    public boolean isEmpty() {
-        return queue.isEmpty();
+    public boolean isEmpty() {   // Method for checking if the queue is empty using the isEmpty method of MyArrayList
+        return queue.isEmpty();   //  @return true if the queue is empty, false otherwise
     }
 
-    public void enqueue(E element) {
-        queue.add(element);
+    public void enqueue(E element) {    // Method for adding an element to the back of the queue using the add method of MyArrayList
+        queue.add(element);    // @param element the element to be added to the back of the queue
     }
 
-    public E dequeue() {
+    public E dequeue() {    // Method for removing and returning the front element of the queue using the removeFirst method of MyArrayList
         if (isEmpty()) {
-            throw new NoSuchElementException("Queue is empty");
+            throw new NoSuchElementException("Queue is empty");    //  @throws NoSuchElementException if the queue is empty
         }
-        return queue.remove(0);
+        return queue.remove(0);     // @return the front element of the queue that was removed
     }
 
-    public E peek() {
-        if (isEmpty()) {
-            throw new NoSuchElementException("Queue is empty");
+    public E peek() {      // Method for returning the front element of the queue without removing it using the get method of MyArrayList
+        if (isEmpty()) {    // @return the front element of the queue
+            throw new NoSuchElementException("Queue is empty");  // @throws NoSuchElementException if the queue is empty
         }
         return queue.get(0);
     }
 
-    public int size() {
-        return queue.size();
+    public int size() {     // Method for getting the number of elements in the queue using the size method of MyArrayList
+        return queue.size();  // @return the number of elements in the queue
     }
 }
